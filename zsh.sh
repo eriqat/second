d3eb7a5ff78649d164fb12f1ffc513b56528eb12
eriqat <erikat.cctf@gmail.com>
2021-05-23 03:12:24 -0700

    first commit

---
+++ zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo apt install curl
+sudo apt install git
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+exit
+
+# plugins=(zsh-autosuggestions)
+
+
